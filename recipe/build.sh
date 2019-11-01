e4bdfd96612c78bfe68ce067bd0a473ed7f967a8
bekozi <bekozi@kolabnow.com>
2019-11-01 08:44:42 -0600

    WIP: Revert build test and comment run_test code

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,4 +7,4 @@ cd ${SRC_DIR}/src/addon/ESMPy
 
 ${PYTHON} setup.py build --ESMFMKFILE=${ESMFMKFILE}
 ${PYTHON} setup.py install --record record.txt
-${PYTHON} setup.py test
+#${PYTHON} setup.py test
