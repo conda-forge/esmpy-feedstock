7304487a919e62da0a3581712f4054d32e80cadd
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-11-11 12:34:28 +0100

    Add activate/deactivate scripts
    These are needed to set `$ESMFMKFILE`

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,3 +7,12 @@ cd ${SRC_DIR}/src/addon/esmpy
 
 ${PYTHON} -m pip install .
 
+for shell in sh csh fish
+do
+  for act_deact in activate deactivate
+  do
+    act_deact_dir=${PREFIX}/etc/conda/${act_deact}.d
+    mdkir -p ${act_deact_dir}
+    cp ${RECIPE_DIR}/scripts/${act_deact}.${shell} ${act_deact_dir}/esmpy-${act_deact}.${shell}
+  done
+done
