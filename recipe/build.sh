ed8051cba740df8333a533d6116e9879599b02c9
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-11-30 09:26:11 -0700

    Fix typo
    Co-authored-by: Klaus Zimmermann <klaus.zimmermann@smhi.se>

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,7 @@ do
   for act_deact in activate deactivate
   do
     act_deact_dir=${PREFIX}/etc/conda/${act_deact}.d
-    mdkir -p ${act_deact_dir}
+    mkdir -p ${act_deact_dir}
     cp ${RECIPE_DIR}/scripts/${act_deact}.${shell} ${act_deact_dir}/esmpy-${act_deact}.${shell}
   done
 done
