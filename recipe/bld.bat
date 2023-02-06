bfc22e613b595b164aee7c1f8818494ff8a02d44
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-02-06 14:32:44 +0100

    Add installation of (de-)activation scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,12 +7,9 @@ rmdir /s src\esmpy\fragments
 
 %PYTHON% -m pip install .
 
-rem for shell in sh csh fish
-rem do
-rem   for act_deact in activate deactivate
-rem   do
-rem     act_deact_dir=${PREFIX}/etc/conda/${act_deact}.d
-rem     mkdir -p ${act_deact_dir}
-rem     cp ${RECIPE_DIR}/scripts/${act_deact}.${shell} ${act_deact_dir}/esmpy-${act_deact}.${shell}
-rem   done
-rem done
+for act_deact in activate deactivate
+do
+  act_deact_dir=${PREFIX}/etc/conda/${act_deact}.d
+  mkdir -p ${act_deact_dir}
+  cp ${RECIPE_DIR}/scripts/${act_deact}.bat ${act_deact_dir}/esmpy-${act_deact}.bat
+done
