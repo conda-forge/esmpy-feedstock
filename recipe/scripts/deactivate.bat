41e464c33764d5d4210141ef6351de257e0bbe9b
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-08-27 14:36:42 +0200

    Remove activate/deactivate scripts
    These are not useful when the package is `noarch: python`, so they
    have been moved to ESMF:
    https://github.com/conda-forge/esmf-feedstock/pull/106

--- recipe/scripts/deactivate.bat
+++
@@ -1,7 +0,0 @@
-:: Restore previous GDAL env vars if they were set.
-
-@set "ESMFMKFILE="
-@if defined _CONDA_SET_ESMFMKFILE (
-  set "ESMFMKFILE=%_CONDA_SET_ESMFMKFILE%"
-  set "_CONDA_SET_ESMFMKFILE="
-)
--- recipe/scripts/deactivate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Restore previous env vars if they were set.
-unset ESMFMKFILE
-
-if [ -n "${_CONDA_SET_ESMFMKFILE:-}" ]; then
-    export ESMFMKFILE="${_CONDA_SET_ESMFMKFILE}"
-    unset _CONDA_SET_ESMFMKFILE
-fi
